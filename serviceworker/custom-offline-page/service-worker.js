f02473ed15e47280a476f191ba8a623bb12f4430
fx <fx.fangxiang@gmail.com>
2015-12-02 17:27:35 +0800

    d

--- serviceworker/custom-offline-page/service-worker.js
+++ serviceworker/custom-offline-page/service-worker.js
@@ -24,7 +24,7 @@ self.addEventListener('install', function(event) {
   
   console.log('serviceworker install'); 
   
-  /*
+  
   const offlineRequest = new Request(OFFLINE_URL);
   event.waitUntil(
     fetch(offlineRequest).then(function(response) {
@@ -33,8 +33,7 @@ self.addEventListener('install', function(event) {
         return cache.put(offlineRequest, response);
       });
     })
-  );  
-  */
+  );    
 });
 
 /*
