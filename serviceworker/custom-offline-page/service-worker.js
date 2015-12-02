bbfaab34baa768647583a6130aa3ef49588a739e
fx <fx.fangxiang@gmail.com>
2015-12-02 17:46:41 +0800

    fix bug

--- serviceworker/custom-offline-page/service-worker.js
+++ serviceworker/custom-offline-page/service-worker.js
@@ -37,10 +37,16 @@ self.addEventListener('install', function(event) {
 
 
 self.addEventListener('fetch', function(event) {
+
+
+  console.log('fetch event for ' + event.request.url);
+
   // We only want to call event.respondWith() if this is a GET request for an HTML document.
   if (event.request.method === 'GET' &&
       event.request.headers.get('accept').includes('text/html')) {
-    console.log('Handling fetch event for', event.request.url);
+	  
+    console.log('Handling fetch event for ' + event.request.url);
+	
     event.respondWith(
       fetch(event.request).catch(function(e) {
         // The catch is only triggered if fetch() throws an exception, which will most likely
@@ -52,7 +58,8 @@ self.addEventListener('fetch', function(event) {
         // Normally, fetch() will consult the browser's HTTP caches before attempting a
         // network request, so in order to trigger offline failure for this sample, we had to
         // use a cache-busting URL parameter to avoid the cache.
-        console.error('Fetch failed; returning offline page instead.' + e);
+        console.log('Fetch failed; returning offline page instead.' + e);
+		
         return caches.open(OFFLINE_CACHE).then(function(cache) {
           return cache.match(OFFLINE_URL);
         });
