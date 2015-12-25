43ac6d9a99b03ce6fe9975aa6a6dab4b19f9de3b
fx <fx.fangxiang@gmail.com>
2015-12-25 16:54:14 +0800

    dd

--- test/service-worker.js
+++ test/service-worker.js
@@ -28,12 +28,10 @@ self.addEventListener('install', function(event) {
 
 
 
-
 self.addEventListener('fetch', function(event) {
 
   console.log('fetch event for ' + event.request.url);
 
-  /*
   event.respondWith(
     
     caches.match(event.request).then(function(response) {
@@ -67,7 +65,7 @@ self.addEventListener('fetch', function(event) {
       });
     })
   );
-  */
+  
 });
 
 
