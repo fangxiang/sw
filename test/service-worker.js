9b7fd6ae8d792d58f4b0a027df529f013538b9c9
fx <fx.fangxiang@gmail.com>
2015-12-25 16:44:13 +0800

    dd

--- test/service-worker.js
+++ test/service-worker.js
@@ -27,6 +27,51 @@ self.addEventListener('install', function(event) {
 });
 
 
+
+
+self.addEventListener('fetch', function(event) {
+
+  console.log('fetch event for ' + event.request.url);
+
+  event.respondWith(
+    
+    caches.match(event.request).then(function(response) {
+    
+ 	  if (response) {
+        console.log('Found response in cache:'+ response);
+        return response;
+      }
+
+      console.log('No response found in cache. About to fetch from network...');
+
+      return fetch(event.request).then(function(response) {
+	  
+        console.log('Response from network is:' + response);
+		
+		if (response.status < 400) {		
+		    caches.open(OFFLINE_CACHE).then(function(cache) {
+		        return cache.put(event.request, response);
+		    });  
+        }
+			
+        return response.clone();
+      
+	  }).catch(function(error) {
+        // This catch() will handle exceptions thrown from the fetch() operation.
+        // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
+        // It will return a normal response object that has the appropriate error code set.
+        console.error('Fetching failed:' + error);
+
+        throw error;
+      });
+    })
+  );
+  
+});
+
+
+
+
 self.addEventListener('fetch', function(event) {
 
   console.log('fetch event for ' + event.request.url);
