c02d168038e3878a0de157d911bdfc2eeb65948f
fx <fx.fangxiang@gmail.com>
2015-12-30 17:27:31 +0800

    dd

--- test/service-worker.js
+++ test/service-worker.js
@@ -5,16 +5,62 @@
 var OFFLINE_CACHE = 'offline';
 var OFFLINE_URL = 'offline.html';
 
+var CACHE_VERSION = 1;
+var CURRENT_CACHES = {
+  prefetch: 'prefetch-cache-v' + CACHE_VERSION
+};
+
 
 self.addEventListener('activate', function(e) {
   console.log('Activate event:' + e);
 });
 
+
+
 self.addEventListener('install', function(event) {
   
-  console.log('serviceworker install'); 
-});
+  console.log("sw install");
+  
+  var urlsToPrefetch = [
+    'images/img.jpg'
+  ];
 
+  console.log('Handling install event. Resources to pre-fetch:', urlsToPrefetch);
+
+  event.waitUntil(
+    caches.open(CURRENT_CACHES.prefetch).then(function(cache) {
+        
+	var cachePromises = urlsToPrefetch.map(function(urlToPrefetch) {
+		
+        var url = new URL(urlToPrefetch, location.href);
+        
+		console.log("fetch url:" + url);
+		
+        return fetch(new Request(url, {mode: 'no-cors'})).then(function(response) {
+		
+		  console.log("fetch response:" + response.status);  
+		
+          if (response.status >= 400) {
+            throw new Error('request for ' + urlToPrefetch +
+              ' failed with status ' + response.statusText);
+          }
+
+          return cache.put(urlToPrefetch, response);
+		  
+        }).catch(function(error) {
+          console.error('Not caching ' + urlToPrefetch + ' due to ' + error);
+        });
+      });
+
+      return Promise.all(cachePromises).then(function() {
+        console.log('Pre-fetching complete.');
+      });
+	  
+    }).catch(function(error) {
+        console.error('Pre-fetching failed:', error);
+    })
+  );
+});
 
 self.addEventListener('fetch', function(event) {
 
