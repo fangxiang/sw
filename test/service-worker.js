25e8841fa17fdbe808b6f41ebad2a8ef731e8804
fx <fx.fangxiang@gmail.com>
2015-12-30 16:58:06 +0800

    dd

--- test/service-worker.js
+++ test/service-worker.js
@@ -1,6 +1,11 @@
 
 'use strict';
 
+
+var OFFLINE_CACHE = 'offline';
+var OFFLINE_URL = 'offline.html';
+
+
 self.addEventListener('activate', function(e) {
   console.log('Activate event:' + e);
 });
@@ -10,7 +15,7 @@ self.addEventListener('install', function(event) {
   console.log('serviceworker install'); 
 });
 
-/*
+
 self.addEventListener('fetch', function(event) {
 
   console.log('fetch event for ' + event.request.url);
@@ -20,9 +25,30 @@ self.addEventListener('fetch', function(event) {
 	fetch(event.request).then(function(response) {
         console.log('Response from network is:' + response);			
         return response;
-	});	
+	})
 	
   );  
 });
+
+
+
+/*
+self.addEventListener('fetch', function(event) {
+
+    console.log('fetch event for ' + event.request.url);
+ 
+    event.respondWith(
+      fetch(event.request).catch(function(e) {
+
+  	    console.log('Fetch failed; returning offline page instead.' + e);
+		
+        return caches.open(OFFLINE_CACHE).then(function(cache) {
+          return cache.match(OFFLINE_URL);
+        });
+      })
+    );
+ 
+});
 */
+
 
